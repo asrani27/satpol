3b77692c9cffa1f72e389d691086636a3f3bfb30
asrani <asrani.27@gmail.com>
2023-11-29 17:55:56 +0800

    f

--- resources/views/superadmin/keluhanwa/index.blade.php
+++ resources/views/superadmin/keluhanwa/index.blade.php
@@ -13,8 +13,7 @@
             <div class="card-header">
                 <h3 class="card-title">DATA KELUHAN VIA BOT WA</h3>
                 <div class="card-tools">
-                    <a href="/pegawai/create" type="button" class="btn bg-gradient-secondary btn-sm">
-                        <i class="fa fa-plus"></i> TAMBAH</a>
+                    
                 </div>
             </div>
             <!-- /.card-header -->
